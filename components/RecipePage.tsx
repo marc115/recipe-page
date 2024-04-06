c6cb28b2cda45eea48537bce18fa001d4bab25ba
Juan Marcano <marchanoth@protonmail.com>
2024-04-06 17:24:13 -0500

    details

--- components/RecipePage.tsx
+++ components/RecipePage.tsx
@@ -3,7 +3,7 @@ import {youngSerif} from '@/app/fonts'
 
 export const RecipePage = () => {
     return (
-        <div className='bg-white p-10 md:rounded-xl flex flex-col gap-y-7 md:w-6/12 w-full md:my-10 text-dark-charcoal'>
+        <div className='bg-white p-10 md:rounded-xl flex flex-col gap-y-7 md:w-6/12 w-full md:my-20 text-dark-charcoal'>
             <Image
                 src='/image-omelette.jpeg'
                 className='rounded-lg'
@@ -36,6 +36,8 @@ export const RecipePage = () => {
                     <li>2-3 large eggs</li>
                     <li>Salt, to taste</li>
                     <li>Pepper, to taste</li>
+                    <li>1 tablespoon of butter or oil</li>
+                    <li>Optional fillings: cheese, diced vegetables, cooked meats, herbs</li>
                 </ul>
             </div>
 
@@ -49,8 +51,16 @@ export const RecipePage = () => {
                     </li>
                     <li><b>Heat the pan:</b> Place a non-stick frying pan over medium heat and add butter or oil.
                     </li>
-                    <li><b>Beat the eggs:</b> In a bowl, beat the eggs with a pinch of salt and pepper until
-                        they are well mixed. You can add a tablespoon of water or milk for a fluffier expirience.
+                    <li><b>Cook the omelette:</b> Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to
+                    ensure the eggs evenly coat the surface.
+                    </li>
+                    <li><b>Add fillings (Optional):</b> When the eggs begin to set at the edges but are still slightly runny
+                    in the middle, sprinkle your chosen fillings over one half of the omelette.
+                    </li>
+                    <li><b>Fold and serve:</b> As the omelette continues to cook, carefully lift one edge and fold it over the
+                    fillings. Let it cook for another minute, then slide it onto a plate.
+                    </li>
+                    <li><b>Enjoy:</b> Serve hot, with additional salt and pepper if needed.
                     </li>
                 </ol>
             </div>
